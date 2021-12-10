fbc54b996788709a34d9445cdb344ab27ec0c2a9
Nay <nay@nay-VirtualBox.com>
2021-12-10 09:40:47 -0600

    Final

--- main.py
+++ main.py
@@ -6,10 +6,24 @@
 from objetoSeguro import ObjetoSeguro
 
 if __name__ == '__main__':
+        # Se instancia el Obj1
     Obj1 = ObjetoSeguro("Obj1")
+        # Se instancia el Obj2
     Obj2 = ObjetoSeguro("Obj2")
-    Obj2.saludar(Obj2.nombre,Obj2.cifrar_msj(Obj1.llave_publica(), "Hola Mundo"))
-    Obj2.almacenar_msj("Hola mundo")
-    Obj2.almacenar_msj("Segundo mensaje")
-    Obj2.almacenar_msj("Tercer mensaje")
-    Obj2.consultar_msj(0)
+
+# Comunicacion Obj2 -> Obj1
+        # Obj2 pide la llave publica de Obj1 para comunicarse con el
+    llavepublicaObj1 = Obj1.llave_publica()
+        # Obj2 cifra el mensaje que quiere enviar a Obj1 con su llave publica (Obj1)
+    mensaje_cifrado = Obj2.cifrar_msj(llavepublicaObj1, "Hola Objeto1 soy Objeto2")
+        # Obj2 envia un saludo a Obj1 y Obj1
+    Obj1.saludar(Obj2.nombre, mensaje_cifrado)
+
+# Comunicacion Obj1 -> Obj2
+        # Obj1 pide la llave publica de Obj2 para comunicarse con el
+    llavepublicaObj2 = Obj2.llave_publica()
+        # Obj1 cifra el mensaje que quiere enviar a Obj2 con su llave publica (Obj2)
+    mensaje_cifrado = Obj1.cifrar_msj(llavepublicaObj2, "Hola Objeto2 yo soy Objeto1")
+        # Obj2 envia un saludo a Obj1 y Obj1
+    Obj2.saludar(Obj1.nombre, mensaje_cifrado)
+
--- objetoSeguro.py
+++ objetoSeguro.py
@@ -8,86 +8,110 @@ from Crypto.Cipher import PKCS1_OAEP
 import base64
 import logging
 
-logging.basicConfig(format='DEBUG : %(message)s',level=logging.DEBUG)
+logging.basicConfig(format='DEBUG : %(message)s', level=logging.DEBUG)
 
 KEY_SIZE = 32  # Para AES-256
 
+
+# Clase ObjetoSeguro
 class ObjetoSeguro:
     def __init__(self, nombre: str):
         # Atributos de la clase ObjetoSeguro
         self.nombre = nombre
-        self.llavePublica, self.llavePrivada = self.__gen_llaves()
-        self.conteoMensajes = 0
-        self.mensajeRecibido = ""
+        self.llavePublica, self.__llavePrivada = self.__gen_llaves()
+        self.__conteoMensajes = 0
+        self.__mensajeRecibido = ""
 
     # Métodos del ObjetoSeguro
-    def __gen_llaves(self):
+    # Metodo privado que, al instanciar el objeto genera la llave publica y privada
+    @staticmethod
+    def __gen_llaves():
         secreto = RSA.generate(1024)
         privada = secreto.exportKey()
         publica = secreto.publickey().exportKey()
         return publica, privada
 
-    def __codificar64(self, msj: str) -> bytes:
+    # Metodo privado que codifica el str ascii ingresado a byte de base64
+    @staticmethod
+    def __codificar64(msj: str) -> bytes:
         aux = base64.b64encode(msj.encode('ascii'))
         return aux
 
-    def __decodificar64(self, msj: bytes) -> str:
+    # Metodo privado que decodifica byte de base64 a string ascii
+    @staticmethod
+    def __decodificar64(msj: bytes) -> str:
         aux = base64.b64decode(msj)
         return aux.decode("ascii")
 
-    def cifrar_msj(self, pub_key: str, msj: str) -> bytes:
-        llavePublica = RSA.importKey(pub_key)
-        llavePublica =PKCS1_OAEP.new(llavePublica)
-        textoCifrado = llavePublica.encrypt(self.__codificar64(msj))
-        return textoCifrado
-
-    def descifrar_msj(self, msj: bytes) -> bytes:
-        llavePrivada = RSA.importKey(self.llavePrivada)
-        llavePrivada = PKCS1_OAEP.new(llavePrivada)
-        textoDescifrado = self.__decodificar64(llavePrivada.decrypt(msj))
-        return textoDescifrado
-
-    def llave_publica(self) -> str:
-        return self.llavePublica
-
-    def saludar(self, name: str, msj: str):
-        self.mensajeRecibido = msj
-        print(f"Hola soy {name} y me quiero comunicar contigo")
-        print(f"Te envio el mensaje cifrado: {msj}")
-        self.responder(name)
-        return
-
-    def responder(self, msj: str) -> bytes:
-        print((f"Hola {msj} recibi tu mensaje"))
-        aux = self.mensajeRecibido + self.__codificar64("MensajeRespuesta")
-        return aux
-
-    def almacenar_msj(self, msj: str) -> dict:
+    # Metodo privado que almacena un mensaje recibido en un archivo de texto
+    def __almacenar_msj(self, msj: str) -> dict:
         aux = dict(
-            ID = self.conteoMensajes,
-            MSJ = msj
+            ID=self.conteoMensajes,
+            MSJ=msj
         )
         self.conteoMensajes += 1
         logging.debug("{}".format(aux))
-        archivo = open("MensajesRecibidos.txt","a")
+        archivo = open(f"RegistoMsj_{self.nombre}.txt", "a")
         with archivo as f:
             print(f"ID: {aux['ID']}, MSJ: {aux['MSJ']}", file=f)
         archivo.close()
-        return
+        return aux
 
-    def consultar_msj(self, id: int) -> dict:
-        a = 0
-        archivo = open("MensajesRecibidos.txt","r")
+    # Metodo privado que almacena un mensaje recibido en un archivo de texto
+    @staticmethod
+    def __consultar_msj(id: int) -> dict:
+        archivo = open("RegistoMsj_{self.nombre}.txt", "r")
         with archivo as f:
             for texto in f:
-                linea = texto.split(",")
-                extraeID = linea[0].split(":")
-                extraeTXT = linea[1].split(":")
-                if int(extraeID[1]) == id:
-                    print(f"ID {id}: {extraeTXT[1]}")
+                linea = texto.split(",", maxsplit=1)
+                extrae_id = linea[0].split(":")
+                extrae_txt = linea[1].split(":")
+                if int(extrae_id[1]) == id:
+                    print(f"ID {id}: {extrae_txt[1]}")
+        aux = dict(
+            ID=id,
+            MSJ=extrae_txt[1]
+        )
+        return aux
+
+    # Metodo publico que cifra un mensaje con una llave publica
+    def cifrar_msj(self, pub_key: str, msj: str) -> bytes:
+        llave_publica = RSA.importKey(pub_key)
+        llave_publica = PKCS1_OAEP.new(llave_publica)
+        texto_cifrado = llave_publica.encrypt(self.__codificar64(msj))
+        return texto_cifrado
+
+    # Metodo publico que descifra un mensaje con una llave publica
+    def descifrar_msj(self, msj: bytes) -> bytes:
+        llave_privada = RSA.importKey(self.__llavePrivada)
+        llave_privada = PKCS1_OAEP.new(llave_privada)
+        texto_descifrado = llave_privada.decrypt(msj)
+        return texto_descifrado
+
+    # Metodo publico con el que se obtiene la llave publica del objeto
+    # No es string por que la biblioteca crea las llaves con otro formato
+    def llave_publica(self):
+        return self.llavePublica
+
+    # Metodo publico que recibe un saludo con un mensaje cifrado
+    def saludar(self, name: str, msj: str):
+        self.mensajeRecibido = msj
+        print(f"Hola soy {name} y me quiero comunicar contigo")
+        # print(f"Te envio el mensaje cifrado: {msj}")
+        self.esperar_respuesta(self.responder(name))
         return
 
+    # Metodo publico que procesa una respuesta a un saludo recibido
+    def responder(self, msj: str) -> bytes:
+        print(f"Hola {msj} recibi tu mensaje")
+        mensaje_respuesta = self.__codificar64("MensajeRespuesta")
+        return mensaje_respuesta + self.mensajeRecibido
+
     def esperar_respuesta(self, msj: bytes):
-        print("Recibi una respuesta")
-        self.almacenar_msj(self.__decodificar64(self.descifrar_msj(msj)))
+        print("Procesando una respuesta")
+        mensaje_respuesta = self.__codificar64("MensajeRespuesta")
+        mensaje_codificado = msj.lstrip(mensaje_respuesta)
+        # print(self.decodificar64(self.descifrar_msj(mensaje_codificado)))
+        self.__almacenar_msj(self.__decodificar64(self.descifrar_msj(mensaje_codificado)))
         return
+    
